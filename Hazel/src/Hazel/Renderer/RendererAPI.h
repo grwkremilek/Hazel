d5acc4935d141c79eb5f4290744d7b7b4870a10e
grw.kremilek <grwkremilek@users.noreply.github.com>
2019-10-03 22:11:19 +0200

    window resizing

--- Hazel/src/Hazel/Renderer/RendererAPI.h
+++ Hazel/src/Hazel/Renderer/RendererAPI.h
@@ -14,8 +14,9 @@ namespace Hazel {
 		{
 			None = 0, OpenGL = 1
 		};
-	public:																//interface for different platforms
+	public:															//interface for different platforms
 		virtual void Init() = 0;
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
 		virtual void SetClearColor(const glm::vec4& color) = 0;
 		virtual void Clear() = 0;
 
