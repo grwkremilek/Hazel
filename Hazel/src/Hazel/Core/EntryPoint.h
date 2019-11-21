41e5ca68994854ef5293102ad959f89eb9bc3223
grw.kremilek <grwkremilek@users.noreply.github.com>
2019-11-21 19:33:24 +0100

    visual profiling: Chrome tracer

--- Hazel/src/Hazel/Core/EntryPoint.h
+++ Hazel/src/Hazel/Core/EntryPoint.h
@@ -16,14 +16,19 @@ extern "C" { _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001; }		// 
 
 int main(int argc, char** argv)													//**
 {
-	Hazel::Log::Init();															//initiate logging system
-	HZ_CORE_WARN("Initialized Log!");
-	std::string a = "The logging system has been initalized.";
-	HZ_INFO("Hello!", a);
+	Hazel::Log::Init();
 
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
 	auto app = Hazel::CreateApplication();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
 	app->Run();
+	HZ_PROFILE_END_SESSION();
+
+	HZ_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Shutdown.json");
 	delete app;
+	HZ_PROFILE_END_SESSION();
 }
 #endif
 
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -2,7 +2,6 @@
 
 #include "Hazel.h"
 
-
 class Sandbox2D : public Hazel::Layer
 {
 public:
@@ -18,19 +17,11 @@ public:
 private:
 	Hazel::OrthographicCameraController m_CameraController;
 
-	//Temp
+	// Temp
 	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 	Hazel::Ref<Hazel::Shader> m_FlatColorShader;
 
 	Hazel::Ref<Hazel::Texture2D> m_CheckerboardTexture;
-
-	struct ProfileResult
-	{
-		const char* Name;
-		float Time;
-	};
-
-	std::vector<ProfileResult> m_ProfileResults;
 
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
